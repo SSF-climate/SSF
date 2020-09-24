6f242b9a6f2be1c4daf140c529f190f32444a645
Xinyan Li <lixx1166@umn.edu>
2020-09-24 16:19:45 -0500

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,10 +1,2 @@
-from .abstractmodel import create_model
-from .fullconn import FNN,create_fully_connected_model
-from .baseline import random,average
-from .recnet import LSTM, LSTM_seq
-from .convnet import ConvNet
-from .autoencoder import Encoder,Decoder,Autoencoder_gpu,Autoencoder_multitask,Autoencoder_multitask_epsilon,Autoencoder_multitask_quad_loss,Autoencoder_multitask_gpu,Autoencoder_seq,Autoencoder_combined,Attention,Autoencoder_with_attention,CNN_LSTM,CNN_FNN,CNN_Autoencoder
-from .autoencoder_multitask import Autoencoder_multitask_AR
 from .relunet import ReluNet
-from .xgbquantile import XGBQuantile
 from .xgbmultitask import XGBMultitask
