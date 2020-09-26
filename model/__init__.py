0cc27b34757a9c921d93dd41033985905d866255
SSF-climate <71453650+SSF-climate@users.noreply.github.com>
2020-09-26 16:39:59 -0500

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,7 @@
 from .relunet import ReluNet
 from .xgbmultitask import XGBMultitask
+from .lassomultitask import LassoMultitask
+from .cnn_lstm import *
+from .cnn_fnn import *
 from .autoencoder_multitask_updated import *
 from .autoencoder_multitask_utils import *
