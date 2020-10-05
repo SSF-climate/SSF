d40b703134d29827a0bd0302045c6c1a78d31a43
SSF-climate <ssf.climate@gmail.com>
2020-10-04 21:33:00 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,13 +4,24 @@ import pickle
 
 
 def load_results(filename):
+    '''load a pickle file
+
+    Args:
+    filename: the path + file name for the file to be loaded
+
+    Returns: a pickle file
+    '''
     with open(filename, 'rb') as f:
-        data = pickle.load(f,encoding='bytes')
+        data = pickle.load(f, encoding='bytes')
     return data
 
 
-def save_results(filename,results):
+def save_results(filename, results):
+    '''save a pickle file
+
+    Args:
+    filename: the path + file name for the file to be saved
+    results: the data array to be saved
+    '''
     with open(filename, 'wb') as fh:
         pickle.dump(results, fh)
-
-
