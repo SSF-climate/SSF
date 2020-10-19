63133e2747e6e90c70e5e6c016c4821683a1bd51
Gil Rosenthal <gil.a.rosenthal@gmail.com>
2020-10-19 16:02:45 -0500

    Update cfg_target.py

--- cfg_target.py
+++ cfg_target.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 ################### Configuration for Data Loading ################################
-path = '/glade/scratch/hexxx/data/'  # need to change to the absolute path of the data files
-path_save = 'SSF/data/'  # need to change to the absolute path to save data_files
-absolute_path = '/SSF/'  # need to change to the absolute path of the code files
+path = 'data/'  # need to change to the absolute path of the data files
+path_save = '/scratch/grosenthal/'  # need to change to the absolute path to save data_files
+absolute_path = '/home-nfs/grosenthal/willett_climate/SSF/'  # need to change to the absolute path of the code files
 rootpath_cv = absolute_path + 'data/random_cv/'
 forecast_rootpath = absolute_path + 'data/forecast/'
 param_path = absolute_path + 'data/random_cv/cv_results_test/best_parameter/'
